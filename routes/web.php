4da0a489f6996743c606445d3e76475e3251cd3e
Gurpreet <gurpreetkait.codes@gmail.com>
2023-12-09 00:37:51 +0530

    filepond file upload done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TemporaryFileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(TemporaryFileController::class)->group(function(){
+    Route::match(['post','delete'],'temp/upload','index')->name('temporary.upload');
 });
